961c4d7a7c63be4dbdcc80ebbc9b8dd415717e46
Hanyx115 <ahmedhanyx115@gmail.com>
2024-03-08 22:41:04 +0200

    sheet2

---
+++ myint/src/Main.java
@@ -0,0 +1,88 @@
+class MyInteger {
+    private int value;
+
+    public MyInteger(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public boolean isEven() {
+        return value % 2 == 0;
+    }
+
+    public boolean isOdd() {
+        return value % 2 != 0;
+    }
+
+    public boolean isPrime() {
+        if (value <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(value); i++) {
+            if (value % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean equals(int otherValue) {
+        return this.value == otherValue;
+    }
+
+    public boolean equals(MyInteger otherMyInteger) {
+        return this.value == otherMyInteger.getValue();
+    }
+
+    public static boolean isEven(int value) {
+        return value % 2 == 0;
+    }
+
+    public static boolean isOdd(int value) {
+        return value % 2 != 0;
+    }
+
+    public static boolean isPrime(int value) {
+        if (value <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(value); i++) {
+            if (value % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isEven(MyInteger myInteger) {
+        return myInteger.isEven();
+    }
+
+    public static boolean isOdd(MyInteger myInteger) {
+        return myInteger.isOdd();
+    }
+
+    public static boolean isPrime(MyInteger myInteger) {
+        return myInteger.isPrime();
+    }
+}
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.print("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
